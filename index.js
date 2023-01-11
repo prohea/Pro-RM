bec84f0c0cde8c2c3112bcae91cbe1b04695e5f4
Mizuri <pro.1.h@outlook.com>
2023-01-10 17:58:51 -0700

    added index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const questions = [
 		name: "title",
 		message: "What is your project title?",
 	},
-    {
+	{
 		type: "input",
 		name: "description",
 		message: "Describe your project:",
@@ -22,50 +22,69 @@ const questions = [
 		name: "installation",
 		message: "Enter installation instructions:",
 	},
-    {
+	{
 		type: "input",
 		name: "usage",
 		message: "Enter usage information:",
 	},
-    {
+	{
 		type: "input",
 		name: "contribution",
 		message: "Enter contribution guidelines:",
 	},
-    {
+	{
 		type: "input",
 		name: "test",
 		message: "Enter test instructions:",
 	},
-    {
+	{
 		type: "list",
 		name: "licenses",
 		message: "Choose a license:",
-        choices: [
-            "MIT",
-            "Mozilla",
-            "Eclipse",
-            "Apache"
-        ]
+		choices: ["MIT", "Mozilla", "Eclipse", "Apache"],
 	},
-    {
+	{
 		type: "input",
 		name: "name",
 		message: "Enter your GitHub username:",
 	},
-    {
+	{
 		type: "input",
 		name: "email",
 		message: "Enter your email address:",
 	},
-   
 ];
 
 //Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+	return new Promise((resolve, reject) => {
+		fs.writeFile(fileName, data, (err) => {
+			if (err) {
+				reject(err);
+				return;
+			}
+
+			resolve({
+				ok: true,
+				message: "File created!",
+			});
+		});
+	});
+}
 
 //Create a function to initialize app
-function init() {}
+function init() {
+	return inquirer.prompt(questions);
+}
 
 // Function call to initialize app
-init();
+init()
+	.then((answers) => {
+		return writeToFile("./dist/README.md", generateMarkdown(answers));
+	})
+	.then((writeFileResponse) => {
+		console.log(writeFileResponse);
+	})
+	.catch((err) => {
+		console.log(err);
+	});
